5fad7602c8e9fe64e94f7a47ebfabd080512e038
Ceccotz <riccardo.ceccotti05@gmail.com>
2016-11-21 12:58:49 +0100

    Add files via upload

---
+++ E_4 Biglietto Mirabilandia.c
@@ -0,0 +1,46 @@
+//Es. N°4
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+/*Alunno Riccardo Ceccotti
+Classe 3°INA
+Data 21/11/2016
+Versione 1.0*/
+Main()
+{
+	int altezza; //variabile che indica l'altezza
+	int età; //variabile che indica l'età
+	int disabile; //variabile che indica se la persona è disabile
+	float prezz_bigl; //variabile che indica il prezzo del biglietto
+	int 1; // variabile che indica che la persona è disabile
+	int 0; //variabile che indica che la persona non è disabile
+	printf("è disabile? (si=1) (no=0)");
+	scanf("%d,&disabile");
+	printf("altezza della persona");
+	scanf("%d,&altezza");
+	print("età della persona");
+	scanf("%d",&età);
+	if(disabile==1) {  // controllo se la persona è disabile allora il prezzo del biglietto è gratis
+		prezz_bigl=gratis;
+	}
+	else {
+		if(altezza<=100); {      // se l'altezza è uguale a 100 allora il prezzo è gratis
+			prezz_bigl=gratis;
+		}
+		if(altezza<=140) and (altezza>=100) {  //se l'altezza è compresa tra 100 e 140 allora il prezzo è 28,00€ senno il prezzo è 34,90€
+			prezz_bigl=28,00€;
+		}
+		else {
+			prezz_bigl=34,90€;
+		}
+		if(età>60) {
+			prezz_bigl=25,00€;
+		}
+	}
+	printf("\nil prezzo del biglietto è:",&prezz_bigl)
+	program("PAUSE");
+	
+	
+
+
+
