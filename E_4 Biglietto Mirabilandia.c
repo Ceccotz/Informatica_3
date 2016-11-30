604fb0d3d23a4268bf550c738da14ae35feeaa2e
Ceccotz <riccardo.ceccotti05@gmail.com>
2016-11-30 17:02:15 +0100

    Update E_4 Biglietto Mirabilandia.c

--- E_4 Biglietto Mirabilandia.c
+++ E_4 Biglietto Mirabilandia.c
@@ -1,45 +1,49 @@
-//Es. N°4
+//Es. NÂ°4
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
 /*Alunno Riccardo Ceccotti
-Classe 3°INA
+Classe 3Â°INA
 Data 21/11/2016
 Versione 1.0*/
-Main()
+main ()
 {
-	int altezza; //variabile che indica l'altezza
-	int età; //variabile che indica l'età
-	int disabile; //variabile che indica se la persona è disabile
-	float prezz_bigl; //variabile che indica il prezzo del biglietto
-	int 1; // variabile che indica che la persona è disabile
-	int 0; //variabile che indica che la persona non è disabile
-	printf("è disabile? (si=1) (no=0)");
-	scanf("%d,&disabile");
-	printf("altezza della persona");
-	scanf("%d,&altezza");
-	print("età della persona");
-	scanf("%d",&età);
-	if(disabile==1) {  // controllo se la persona è disabile allora il prezzo del biglietto è gratis
-		prezz_bigl=gratis;
+	float disabile;		//Disabile
+	float accompagnatore;	//Accompagnatore
+	float altezza;		//Altezza
+	float prezzo;		//Prezzo
+	float etÃ ;		//etÃ 
+	printf("Ã© un disabile? (1=Si, 0=No) ");	 							
+	scanf("%f",&disabile);
+	if(disabile==0){																		
+		printf("Ã© un accompagnatore? (1=Si, 0=No) ");	 				
+		scanf("%f",&accompagnatore);													
+		if (accompagnatore==0){																	
+			printf ("Inserire il valore dell'etÃ ");								
+			scanf("%f",&etÃ );	
+			if (etÃ >60){																
+				prezzo=25;														                                                  
+			}
+			else{																	
+				printf("Inserire il valore dell'altezza ");										
+				scanf("%f",&altezza);
+				if(altezza<=100){														
+					prezzo=0;															
+				}
+				else{																
+					if(altezza<=140){													
+						prezzo=28;														
+	                                }
+					else{															
+						prezzo=34.90;												 		
+																		
+					}
+									
+		
+		}
+																	
 	}
-	else {
-		if(altezza<=100); {      // se l'altezza è uguale a 100 allora il prezzo è gratis
-			prezz_bigl=gratis;
-		}
-		if(altezza<=140) and (altezza>=100) {  //se l'altezza è compresa tra 100 e 140 allora il prezzo è 28,00€ senno il prezzo è 34,90€
-			prezz_bigl=28,00€;
-		}
-		else {
-			prezz_bigl=34,90€;
-		}
-		if(età>60) {
-			prezz_bigl=25,00€;
-		}
-	}
-	printf("\nil prezzo del biglietto è:",&prezz_bigl)
-	program("PAUSE");
-	
+	printf("il prezzo Ã©:%f",prezzo);	
 	
 
 
