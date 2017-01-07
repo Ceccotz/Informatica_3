b8aa84456e71b5a28bb4a87759fe5c855bbd863c
Ceccotz <riccardo.ceccotti05@gmail.com>
2017-01-07 15:52:58 +0100

    Add files via upload

---
+++ E_6 Calcolo del fattoriale di un numero.c
@@ -0,0 +1,22 @@
+//Dato un numero calcolare il suo fattoriale
+int main() {
+int num; //specifica il valore del numero
+int i; //specifica il valore del contatore
+int fat; //specifica il valore del fattoriale
+int N; 
+/*Alunno:Riccardo Ceccotti
+Classe:3°INA
+Data:28/12/2016
+Versione:1.0 */
+printf("inserire il valore di num");
+scanf("%d",&num);
+fat=1;
+i=0;  
+while(i<num) {
+             fat=fat*(N-1);
+             i=i+1;
+}
+printf("il valore di fat è:%d",&fat);
+}             
+             
+                 
