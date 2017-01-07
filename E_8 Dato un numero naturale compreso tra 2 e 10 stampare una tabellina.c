c374c1ca9f7bc1f5129e68e28cb43c51dd5e95a8
Ceccotz <riccardo.ceccotti05@gmail.com>
2017-01-07 15:53:32 +0100

    Add files via upload

---
+++ E_8 Dato un numero naturale compreso tra 2 e 10 stampare una tabellina.c
@@ -0,0 +1,17 @@
+//Dato un numero naturale compreso tra 2 e 10 stampare una tabellina
+int main() {
+int N; //variabile che specifica il numero
+int i; //specifica il valore del contatore
+/*Alunno:Riccardo Ceccotti
+  Classe:3°INA
+  Data:28/12/2016
+  Versione:1.0 */
+do {
+    printf("inserire iol valore di N");
+    scanf("%d",& N);
+    while (!(N>=0)&&(N<=10))
+   }
+   for(i=0;i<=10;1=i+1) {
+                        printf(N*i);
+                        }
+}                              
