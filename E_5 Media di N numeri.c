afcbebf2b44173f1d34379aea3b78ae70dd709e8
Ceccotz <riccardo.ceccotti05@gmail.com>
2017-01-08 12:30:06 +0100

    Update E_5 Media di N numeri.c

--- E_5 Media di N numeri.c
+++ E_5 Media di N numeri.c
@@ -6,7 +6,7 @@ int i; //specifica il valore del contatore
 int media; //specifica il valore della media
 int num; //specifica il valore di numero
 /*Alunno:Riccardo Ceccotti
-  Classe:3°INA
+  Classe:3Â°INA
   Data:28/12/2016
   Versione:1.0 */
 printf("inserire il valore di N");
@@ -15,11 +15,12 @@ somma=0;
 i=0;
 while(i<N) {
            printf("inserisci il valore di num");
+           scanf("%d", &num);
            somma=somma+num;
            i=i+1;
            }
 media=somma/N;
-printf("il valore della media è; %d",& media);
+printf("il valore della media Ã¨: %d",& media);
 }           
            
            
