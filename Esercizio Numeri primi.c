fd6486e59f292af96caba7c6a425679ffeddbb82
Ceccotz <riccardo.ceccotti05@gmail.com>
2017-04-26 16:44:49 +0200

    Add files via upload

---
+++ Esercizio Numeri primi.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+/* Titolo: Creare unh programma che dateo un numero intero positivo verifichi se il numero è primo.
+   Studente: Riccardo Ceccotti
+   Classe: 3INA
+   Data: 24/04/2017 
+   Versione: 1.0  */
+
+int main()
+{
+	int n;
+	int primo;
+	int div;
+	
+	printf("Inserisci un numero qualsiasi: ");
+	scanf("%d",&n);
+	printf("\n");
+	primo=0;
+	div=2;
+	
+	while(div<n){
+		if(n % div==0){
+			div=n;
+			primo=1;
+		}
+		div++;
+	}
+	
+	if(primo==0){
+		printf("Il numero inserito e' primo");
+	}else{
+		printf("Il numero inserito non e' primo");
+	}
+}
