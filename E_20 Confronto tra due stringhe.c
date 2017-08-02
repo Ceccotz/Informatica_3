b867d689806a3b43180d7b20815b9f6a26b15ba7
Ceccotz <riccardo.ceccotti05@gmail.com>
2017-08-02 11:02:11 +0200

    Add files via upload

---
+++ E_20 Confronto tra due stringhe.c
@@ -0,0 +1,33 @@
+//E_20
+#include<stdio.h>
+#include<stdlib.h>
+/*Alunno Riccardo Ceccotti
+Classe 3°INA
+Data 02/08/2017
+Versione 1.0*/
+int main () {
+	char p1[100];
+	char p2[100];
+	int i;
+	int uguali;
+	
+	printf("Inserisci la prima parola: ");
+	scanf("%s", p1);
+	printf("Inserisci la seconda parola: ");
+	scanf("%s", p2);
+	
+	i=0;
+	uguali=0;
+	while(uguali==1 && (p1[i]!='\0' || p2[i]!='\0')){
+		if(p1[i]!=p2[i]){
+			uguali=0;
+		}
+		i++;
+	}
+	if(uguali=1){
+		printf("Sono uguali");
+	}else{
+		printf("Non sono uguali");
+	}
+	system("PAUSE");
+}
